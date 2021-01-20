3a4dd290dcdcd86cf1830330e92c5e94233a14eb
leoriq <30530075+leoriq@users.noreply.github.com>
2021-01-20 19:30:24 -0300

    Indo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Header from './components/Header';
 import AddTodo from './components/AddTodo';
+import TodoFeed from './components/TodoFeed';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddTodo />
+      <TodoFeed />
     </div>
   );
 }
---
+++ src/components/TodoFeed/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import './style.css';
+
+import DoneRoundedIcon from '@material-ui/icons/DoneRounded';
+import RemoveRoundedIcon from '@material-ui/icons/RemoveRounded';
+
+class TodoFeed extends Component {
+    state = {
+        feed: [],
+    };
+    async componentDidMount() {
+        const response = await api.get('todos');
+
+        this.setState({ feed: response.data });
+    }
+
+    handleDone = id => {
+        api.post(`todos/${id}`);
+    }
+
+    handleDelete = id => {
+        api.delete(`todos/${id}`);
+    }
+
+    render() {
+        return (
+            <section id="todo-list">
+                <h2>TODO</h2>
+                {this.state.feed.map(todo => (
+                    <article key={todo._id}>
+                        <h3>{todo.todo}</h3>
+                        <div className="actions">
+                            <DoneRoundedIcon />
+                            <RemoveRoundedIcon />
+                        </div>
+                    </article>
+
+
+                ))}
+            </section>
+        )
+    }
+}
+
+export default TodoFeed;
